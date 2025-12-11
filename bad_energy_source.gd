c0c9c54b8c47aeccf01f957337a3d0e2e80f684b
Ronak <ronakfeedback@gmail.com>
2025-12-11 15:51:13 +0000

    New gppd capsule location scene

--- bad_energy_source.gd
+++ bad_energy_source.gd
@@ -47,10 +47,6 @@ func _ready():
 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("player_body"):
 		panel_instance.visible = true
---
+++ good_capsule_spot.gd
@@ -0,0 +1,33 @@
+extends Node3D
+
+
+@export var is_clean: bool = true
+@export var energy_type: String = "Wind"
+@export var co2_value: int = 100
+@export var icon_img: Texture2D
+
+@export var panel_title: String = "Wind Energy"
+@export var panel_body: String = "Wind info"
+@export var panel_color: Color = Color(0.0, 0.708, 0.254, 1.0) # red
+
+@onready var panel = $FloatingInfoPanel
+@onready var capsule = $Capsule
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	capsule.is_clean = is_clean
+	capsule.energy_type = energy_type
+	capsule.co2_value = co2_value
+	capsule.icon_img = icon_img
+	
+	panel.set_content(panel_title, panel_body)
+	panel.set_color(panel_color)
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("player_body"):
+		panel.visible = true
+
+
+func _on_area_3d_body_exited(body):
+	if body.is_in_group("player_body"):
+		panel.visible = false
